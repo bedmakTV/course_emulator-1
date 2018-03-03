41cca7b3e3ddc55f53e42c0293f8872c28cea1c5
rod98 <listner2@gmail.com>
2018-03-03 16:55:15 +0300

    Added selection for config file

--- src/emulator/main.c
+++ src/emulator/main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 #include "virtcpu.h"
 
 int main(int argc, char **argv) {
-	ECM ecm;
-	int err = 0;
+	ECM   ecm;
+	int   err      = 0, i;
+	char  dcname[] = "ecm.cfg";
+	char  dename[] = "a.out";
+	char *confnm   = dcname;
+	char *exefnm   = dename;
 
-	err |= getECfg(&ecm, "ecm.cfg");
+	for (i = 1; i < argc; ++i)
+		if (!strcmp(argv[i], "-c"))
+			confnm = argv[i++ + 1];
+		else
+			exefnm = argv[i];
+
+	err |= getECfg(&ecm, confnm);
 
 	if (!err && argc > 1) {
-		err  = loadExe(&ecm, argv[1]);
+		err  = loadExe(&ecm, exefnm);
 		err |= run    (&ecm);
 		stopECM(&ecm);
 	}
